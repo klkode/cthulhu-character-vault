2a09fec8e719f7f4e6783dde2310ffe9b66d9a9f
klkode <katrinalkite@gmail.com>
2024-03-19 17:24:37 -0400

    Add hook support for getting a session for checking for token authentication.

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -3,12 +3,12 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-  const [session, setSession] = useState(null);
-  return (
-    <AuthContext.Provider value={{ session, setSession }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    const [session, setSession] = useState(null);
+    return (
+        <AuthContext.Provider value={{ session, setSession }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 export default AuthProvider;
