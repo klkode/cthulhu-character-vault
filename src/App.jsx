45b3288ecd3865ff0ed612ad2e47a5a7bcec29ff
klkode <katrinalkite@gmail.com>
2024-03-19 16:11:22 -0400

    Add NotFoundPage for bad paths. Add routes to all the different pages. Add context folder with auth-context.js to handle sharing the session login token being shared througout the site. remove default styling from App.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import AuthProvider from "./context/auth-context";
+
+import RegisterPage from './pages/RegisterPage/RegisterPage.jsx';
+import LoginPage from './pages/LoginPage/LoginPage.jsx';
+import HomePage from './pages/HomePage/HomePage.jsx';
+import CharacterVaultPage from './pages/CharacterVaultPage/CharacterVaultPage.jsx';
+import AddCharacterPage from './pages/AddCharacterPage/AddCharacterPage.jsx';
+import EditCharacterPage from './pages/EditCharacterPage/EditCharacterPage.jsx';
+import CharacterSheetPage from './pages/CharacterSheetPage/CharacterSheetPage.jsx';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage.jsx';
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<RegisterPage />} />
+                    <Route path="/characters" element={<CharacterVaultPage />} />
+                    <Route path="/characters/add" element={<AddCharacterPage />} />
+                    <Route path="/charactes/:id" element={<CharacterSheetPage />} />
+                    <Route path="/charactes/:id/edit" element={<EditCharacterPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+    );
 }
 
 export default App;
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import './NotFoundPage.scss';
+
+function NotFoundPage() {
+    
+  return (
+    <div>404 Not Found Page</div>
+  );
+}
+
+export default NotFoundPage;
