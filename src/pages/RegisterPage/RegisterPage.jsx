e209401d3966f9bab0842d481f410a1896c90d4b
klkode <katrinalkite@gmail.com>
2024-03-24 22:12:19 -0400

    Add heading and buttons to the register page.

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,9 +1,17 @@
+import ButtonLink from '../../components/ButtonLink/ButtonLink';
 import './RegisterPage.scss';
 
 function RegisterPage() {
     
   return (
-    <div>Register Page</div>
+    <section className="register">
+      <h1 className="register__heading">Sign Up</h1>
+        
+      <div className="register__btn-container">
+        <ButtonLink btnText={"Cancel"} navTo={"/"} />
+        <ButtonLink btnText={"Register"} navTo={"/"} />
+      </div>
+    </section>
   );
 }
 
