7545e6869ce65b385f7a93e24ede5a807ad40bad
klkode <katrinalkite@gmail.com>
2024-03-24 22:17:40 -0400

    Add heading and buttons to the login page.

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,18 @@
+import ButtonLink from '../../components/ButtonLink/ButtonLink';
 import './LoginPage.scss';
 
 function LoginPage() {
     
   return (
-    <div>Login Page</div>
+    <section className="log-in">
+    <h1 className="log-in__heading">Log In</h1>
+      
+    <div className="log-in__btn-container">
+      <ButtonLink btnText={"Cancel"} navTo={"/"} />
+      <ButtonLink btnText={"Sign Up"} navTo={"/signup"} />
+      <ButtonLink btnText={"Log In"} navTo={"/"} />
+    </div>
+  </section>
   );
 }
 
