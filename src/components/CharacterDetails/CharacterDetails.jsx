763f1f366c04d537e090c1aaaaaab259f85b6dc9
klkode <katrinalkite@gmail.com>
2024-03-26 17:18:17 -0400

    Add subheading to the CharacterDetails component. Populate CharacterStats component, populate the scss file with components class names, and add basic layout styling.

--- src/components/CharacterDetails/CharacterDetails.jsx
+++ src/components/CharacterDetails/CharacterDetails.jsx
@@ -6,9 +6,10 @@ function CharacterDetails({character}) {
   // TODO span for labels?
   return (
     <article className="character-details">
+      <h3 className="character-details__heading">Details</h3>
       <div className="character-details__container">
-        <h2 className="character-details__name">{`Name: ${character.character_name}`}</h2>
-        <h3 className="character-details__background">{`Background: ${character.background_id}`}</h3> 
+        <h4 className="character-details__name">{`Name: ${character.character_name}`}</h4>
+        <h4 className="character-details__background">{`Background: ${character.background_id}`}</h4> 
       </div>
       <div className="character-details__container">
         <div className="character-details__sub-container">
--- src/components/CharacterStats/CharacterStats.jsx
+++ src/components/CharacterStats/CharacterStats.jsx
@@ -1,9 +1,43 @@
 import './CharacterStats.scss';
 
-function CharacterStats() {
+function CharacterStats({stats}) {
+
     
   return (
-    <div>Character Stats Component</div>
+    <article className="character-stats">
+      <h3 className="character-stats__heading">Stats</h3>
+      <div className="character-stats__container character-stats__container--primary-stats">
+        <div className="character-stats__rolled-grouping">
+          <p className="character-stats__stat">{`STR: ${stats.strength}`}</p>
+          <p className="character-stats__stat">{`DEX: ${stats.dexterity}`}</p>
+          <p className="character-stats__stat">{`CON: ${stats.constitution}`}</p>
+        </div>
+        <div className="character-stats__rolled-grouping">
+          <p className="character-stats__stat">{`SIZE: ${stats.size}`}</p>
+          <p className="character-stats__stat">{`APP: ${stats.appearance}`}</p>
+          <p className="character-stats__stat">{`INT: ${stats.intelligence}`}</p>
+        </div>
+        <div className="character-stats__rolled-grouping">
+          <p className="character-stats__stat">{`EDU: ${stats.education}`}</p>
+          <p className="character-stats__stat">{`POW: ${stats.power}`}</p>
+          <p className="character-stats__stat">{`LUCK: ${stats.luck}`}</p>
+        </div>
+      </div>
+      <div className="character-stats__container">
+        <div className="character-stats__calculated-grouping">
+          <p className="character-stats__stat">{`Health: ${stats.health}`}</p>
+          <p className="character-stats__stat">{`Major Wound Threshold: ${Math.floor(stats.health/2)}`}</p>
+        </div>
+        <div className="character-stats__calculated-grouping">
+          <p className="character-stats__stat">{`Sanity: ${stats.sanity}`}</p>
+          <p className="character-stats__stat">{`Indefinite Insanity Threshold: ${Math.floor(stats.sanity/5)}`}</p>
+        </div>
+        <div className="character-stats__calculated-grouping">
+          <p className="character-stats__stat">{`Magic Points: ${stats.magic_points}`}</p>
+          <p className="character-stats__stat">{`Movement: ${stats.movement}`}</p>
+        </div>
+      </div>
+    </article>
   );
 }
 
