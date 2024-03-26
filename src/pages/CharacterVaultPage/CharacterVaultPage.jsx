d28af06e46993a931de738081f1f901b6ea61467
klkode <katrinalkite@gmail.com>
2024-03-26 09:51:19 -0400

    Setup the ability to make a call to get a list of characters. Sends the authorization token but the backend is still in dummy mode.

--- src/pages/CharacterVaultPage/CharacterVaultPage.jsx
+++ src/pages/CharacterVaultPage/CharacterVaultPage.jsx
@@ -1,9 +1,57 @@
 import './CharacterVaultPage.scss';
+import { useEffect, useState } from 'react';
+import { BASE_URL } from "../../constant-variables.js"
+import axios from "axios";
 
 function CharacterVaultPage() {
-    
+
+  const [characterList, setCharacterList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const token = sessionStorage.getItem("token");
+
+  useEffect(() => {
+    const populateVault = async(token) => {
+      try{
+        const response = await axios.get(`${BASE_URL}characters`, {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        });
+        console.log(response.data);
+        setCharacterList(response.data);
+        setIsLoading(false);
+
+      }catch(error){
+        console.error(error);
+        setCharacterList([]);
+        setIsLoading(false);
+      }
+    }
+
+    if(!!token){
+      populateVault(token);
+    }else{
+      setIsLoading(false);
+    }
+  }, [token]);
+
+  // TODO look nicer
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  // TODO look nicer
+  if(!token){
+    return <div>You must be logged in to view your character vault.</div>
+  }
+
   return (
-    <div>Character Vault Page</div>
+    <section className="character-vault">
+      <h1>Character Vault</h1>
+      <input className="character-vault__search" />
+
+    </section>
   );
 }
 
