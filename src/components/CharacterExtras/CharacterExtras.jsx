424d0a2d05c7adba2cde4c8facfc10b3c12087aa
klkode <katrinalkite@gmail.com>
2024-03-27 09:48:34 -0400

    Import bootstrap library and include in the index file. Create a dropdown for the extra detail categories and display the vaule is a neighbouring p element.

--- src/components/CharacterExtras/CharacterExtras.jsx
+++ src/components/CharacterExtras/CharacterExtras.jsx
@@ -1,9 +1,58 @@
+import { useState } from 'react';
 import './CharacterExtras.scss';
 
-function CharacterExtras() {
+function CharacterExtras({character}) {
+
+  const [selectState, setSelectState] = useState(null);
+
+  function selectionHandler(event){
+    const keyName = event.target.value;
+    setSelectState(character[keyName]);
+  }
     
   return (
-    <div>Character Extras Component</div>
+    <article className="character-extras">
+      <h3 className="character-extras__heading">More Details</h3>
+      <div className="character-extras__container">
+        <div className="dropdown character-extras__dropdown">
+          <button className="btn btn-secondary dropdown-toggle character-extras__option character-extras__option--default" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Select Detail
+          </button>
+          <ul className="dropdown-menu character-extras__dropdown-menu">
+            <li className="character-extras__option-container">
+              <button className="dropdown-item character-extras__option" type="button" value="special_people" onClick={selectionHandler}>Special People</button>
+            </li>
+            <li className="character-extras__option-container">
+              <button className="dropdown-item character-extras__option" type="button" value="favoured_possession" onClick={selectionHandler}>Favoured Posssion</button>
+            </li>
+            <li className="character-extras__option-container">
+              <button className="dropdown-item character-extras__option" type="button" value="traits" onClick={selectionHandler}>Traits</button>
+            </li>
+            {/* TODO more complicated, save for later */}
+            {/* <li className="character-extras__option-container">
+              <button className="dropdown-item character-extras__option" type="button" value="equipment" onClick={selectionHandler}>Equipment</button>
+            </li> */}
+            <li className="character-extras__option-container">
+              <button className="dropdown-item character-extras__option" type="button" value="injuries" onClick={selectionHandler}>Injuries</button>
+            </li>
+            <li className="character-extras__option-container">
+              <button className="dropdown-item character-extras__option" type="button" value="mania" onClick={selectionHandler}>Manias</button>
+            </li>
+            <li className="character-extras__option-container">
+              <button className="dropdown-item character-extras__option" type="button" value="spells" onClick={selectionHandler}>Spells</button>
+            </li>
+            <li className="character-extras__option-container">
+              <button className="dropdown-item character-extras__option" type="button" value="notes" onClick={selectionHandler}>Notes</button>
+            </li>
+          </ul>
+        </div>
+        <div className="character-extras__value-container">
+          {/*TODO so info in nicer way  */}
+          <p className="character-extras__value">{selectState}</p>
+        </div>
+      </div>
+
+    </article>
   );
 }
 
